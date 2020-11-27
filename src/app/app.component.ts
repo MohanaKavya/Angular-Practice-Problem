a0b724a26ff292444ad20b8b7740f15e0690f533
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-11-27 13:15:04 +0530

    Modified : Added reference link for the logo for on click event using event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NewHelloWorld';
-  imgURL = '';
+  imgURL;
+  URL;
   ngOnInit() : void {
     this.title = "Hello from BridgeLabz.";
     this.imgURL = "../assets/blz_logo.png";
+    this.URL = "https://www.bridgelabz.com/"
+  }
+  onClick($event) {
+    console.log("Sava Button is Clicked!",$event);
+    window.open(this.URL, "_blank");
   }
 }
